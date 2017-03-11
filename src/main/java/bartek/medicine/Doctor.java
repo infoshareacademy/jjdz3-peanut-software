f7c9de6b4aa89b6b0cdfae37ec738ac53fc605fc
bartek <bartlomiej.olewinski@ifresearch.org>
2017-03-11 14:04:43 +0100

    bartek-medicine: Doctor class

---
+++ src/main/java/bartek/medicine/Doctor.java
@@ -0,0 +1,42 @@
+package bartek.medicine;
+
+/**
+ * Created by bartman3000 on 2017-03-11.
+ */
+public class Doctor {
+
+    private String name;
+    private String surname;
+    private String specialization;
+
+    public Doctor(String name, String surname, String specialization) {
+        this.name = name;
+        this.surname = surname;
+        this.specialization = specialization;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+}
