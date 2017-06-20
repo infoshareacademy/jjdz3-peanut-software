f515f16d640a5bfcbcaa5d6149f0e9b5153b5834
Mariusz Szymanski <mariusz.szymanski@sayen.pl>
2017-06-20 23:58:34 +0200

    Create new module for rest api

---
+++ rest-api/src/main/java/com/airhacks/JAXRSConfiguration.java
@@ -0,0 +1,15 @@
+package com.airhacks;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * JAX-RS endpoint.
+ *
+ * @author Mariusz Szymanski
+ */
+
+@ApplicationPath("reports")
+public class JAXRSConfiguration extends Application {
+
+}
