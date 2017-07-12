4d5a69f0a89ec5ef4f752cfe2c4dfded8598d4c3
Mariusz Szymanski <mariusz.szymanski@sayen.pl>
2017-07-12 18:44:10 +0200

    Post method to add specialization

--- rest-api/src/main/java/api/SpecializationReportRestService.java
+++ rest-api/src/main/java/api/SpecializationReportRestService.java
@@ -3,11 +3,11 @@ package api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.PreferredSpecializations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Map;
@@ -19,10 +19,13 @@ import java.util.Map;
 @Produces(MediaType.APPLICATION_JSON)
 public class SpecializationReportRestService {
 
+    private Logger LOG = LoggerFactory.getLogger(SpecializationReportRestService.class);
+
     @Inject
     private PreferredSpecializations preferredSpecializations;
 
     @GET
+    @Path("/all")
     public Response getPreferredSpecializations() {
         Map<String, Integer> statistics = preferredSpecializations.getSpecializationsStatistics();
         String json;
@@ -32,5 +35,20 @@ public class SpecializationReportRestService {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         return Response.status(Response.Status.OK).entity(json).build();
+    }
+
+    @POST
+    @Path("/add")
+    public Response setPreferredSpecializations(
+            @QueryParam("specialization") String specialization,
+            @QueryParam("value") Integer value) {
+
+        LOG.info("add/update specialization: " + specialization + " value: " + value);
+
+        Map<String, Integer> statistics = preferredSpecializations.getSpecializationsStatistics();
+        statistics.put(specialization, value);
+        preferredSpecializations.setSpecializationsStatistics(statistics);
+
+        return getPreferredSpecializations();
     }
 }
