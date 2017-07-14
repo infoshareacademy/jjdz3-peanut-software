ab6d969be7483904878751a39df1d02b65abef6d
Mariusz Szymanski <mariusz.szymanski@sayen.pl>
2017-07-14 12:44:22 +0200

    Get method for one chosen specialization

--- rest-api/src/main/java/api/SpecializationReportRestService.java
+++ rest-api/src/main/java/api/SpecializationReportRestService.java
@@ -9,15 +9,17 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Mariusz Szymanski
  */
-@Path("/specializations")
+@Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 public class SpecializationReportRestService {
 
@@ -27,9 +29,28 @@ public class SpecializationReportRestService {
     private PreferredSpecializations preferredSpecializations;
 
     @GET
-    @Path("/all")
+    @Path("/specializations/all")
     public Response getPreferredSpecializations() {
         Map<String, Integer> statistics = preferredSpecializations.getSpecializationsStatistics();
+        String json;
+        try {
+            json = new ObjectMapper().writeValueAsString(statistics);
+        } catch (JsonProcessingException e) {
+            LOG.debug("Json Processing Exception");
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.status(Response.Status.OK).entity(json).build();
+    }
+
+    @GET
+    @Path("/specialization/{specialization}")
+    public Response getPreferredSpecializationQuantity(
+            @PathParam("specialization") String specialization) {
+        Map<String, Integer> statistics = preferredSpecializations
+                .getSpecializationsStatistics()
+                .entrySet().stream()
+                .filter(map -> specialization.equals(map.getKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
         String json;
         try {
             json = new ObjectMapper().writeValueAsString(statistics);
