c7f0f5ff44939afed6533f57ad20cd9f10709ff3
Mariusz Szymanski <mariusz.szymanski@sayen.pl>
2017-07-25 22:38:17 +0200

    fix problem with LocalDateTime in json

--- rest-api/src/main/java/api/UserActivityReportRestService.java
+++ rest-api/src/main/java/api/UserActivityReportRestService.java
@@ -1,5 +1,10 @@
 package api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import domain.UserActivity;
 import domain.UserActivityStorageService;
 
@@ -24,7 +29,20 @@ public class UserActivityReportRestService {
     @GET
     @Path("/usersActivity/all")
     public Response getUsersActivity() {
+
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
+
         List<UserActivity> userActivities = activityStorageService.getAllUsersActivity();
-        return Response.status(Response.Status.OK).entity(userActivities).build();
+
+        try {
+            String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(userActivities);
+            return Response.status(Response.Status.OK).entity(json).build();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 }
