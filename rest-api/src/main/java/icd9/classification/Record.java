044896f4cccb1bcce63d2dc5e2b5efd7035dfd23
Mariusz Szymanski <mariusz.szymanski@sayen.pl>
2017-07-28 23:20:53 +0200

    icd-9 classification model

---
+++ rest-api/src/main/java/icd9/classification/Record.java
@@ -0,0 +1,80 @@
+package icd9.classification;
+
+/**
+ * @author Mariusz Szymanski
+ */
+public class Record {
+
+    private String sectionNumber;
+    private String section;
+    private String subsectionNumber;
+    private String subsection;
+    private String mainCategoryNumber;
+    private String mainCategory;
+    private String detailedCategoryNumber;
+    private String detailedCategory;
+
+    public String getSectionNumber() {
+        return sectionNumber;
+    }
+
+    public void setSectionNumber(String sectionNumber) {
+        this.sectionNumber = sectionNumber;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getSubsectionNumber() {
+        return subsectionNumber;
+    }
+
+    public void setSubsectionNumber(String subsectionNumber) {
+        this.subsectionNumber = subsectionNumber;
+    }
+
+    public String getSubsection() {
+        return subsection;
+    }
+
+    public void setSubsection(String subsection) {
+        this.subsection = subsection;
+    }
+
+    public String getMainCategoryNumber() {
+        return mainCategoryNumber;
+    }
+
+    public void setMainCategoryNumber(String mainCategoryNumber) {
+        this.mainCategoryNumber = mainCategoryNumber;
+    }
+
+    public String getMainCategory() {
+        return mainCategory;
+    }
+
+    public void setMainCategory(String mainCategory) {
+        this.mainCategory = mainCategory;
+    }
+
+    public String getDetailedCategoryNumber() {
+        return detailedCategoryNumber;
+    }
+
+    public void setDetailedCategoryNumber(String detailedCategoryNumber) {
+        this.detailedCategoryNumber = detailedCategoryNumber;
+    }
+
+    public String getDetailedCategory() {
+        return detailedCategory;
+    }
+
+    public void setDetailedCategory(String detailedCategory) {
+        this.detailedCategory = detailedCategory;
+    }
+}
---
+++ rest-api/src/main/java/icd9/classification/Subsection.java
@@ -0,0 +1,31 @@
+package icd9.classification;
+
+import java.util.List;
+
+/**
+ * @author Mariusz Szymanski
+ */
+public class Subsection {
+
+    private String code;
+    private String title;
+    private List<MainCategory> mainCategories;
+
+    public Subsection(String code, String title, List<MainCategory> mainCategories) {
+        this.code = code;
+        this.title = title;
+        this.mainCategories = mainCategories;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<MainCategory> getMainCategories() {
+        return mainCategories;
+    }
+}
